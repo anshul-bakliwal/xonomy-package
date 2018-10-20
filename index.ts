c92c47ec381e4f2cbb4b690329aeef5d8f832ec2
anshul-bakliwal <anshulbakliwal@gmail.com>
2018-10-20 14:21:06 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
-import Xonomy = require("./src/app/xonomy.js");
+import * as Xonomy from "./src/app/xonomy.js";
 import "./src/assets/css/xonomy.css";
 // to get the index.html in the dist
 import 'file-loader?name=[name].[ext]!./src/app/index.html';
-export = Xonomy
+
+
+export default class Xonomy {
+  constructor() {
+  }
+}
